3d511205244eaebc5ba5c600aef65b0eb1adfdb0
Prashant Kumar <pk611886@gmail.com>
2020-08-01 05:13:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@ app_dir = os.path.abspath(os.path.dirname(__file__))
 
 class BaseConfig:
     DEBUG = True
-    POSTGRES_URL = "https://techconfrr1.azurewebsites.net"
-    POSTGRES_USER = "https://techconfrr1.azurewebsites.net"
+    POSTGRES_URL = "techconfsrv.postgres.database.azure.com"
+    POSTGRES_USER = "tc_admin@techconfsrv"
     POSTGRES_PW = "Orange@789"
-    POSTGRES_DB = "testrr1.postgres.database.azure.com"
+    POSTGRES_DB = "techconfdb"
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER, pw=POSTGRES_PW, url=POSTGRES_URL,
                                                           db=POSTGRES_DB)
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    SQLALCHEMY_DATABASE_URI = DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
     SERVICE_BUS_CONNECTION_STRING = 'Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=yUN5LyhL1xYu1/KWQln2Ivm+2umRWboHzQzy189TlFU='
