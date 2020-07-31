92e9c3272e1b323637a878d21d4bfdb62930232e
Prashant Kumar <pk611886@gmail.com>
2020-07-31 14:34:22 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    #return render_template('index.html')
-    return "Hello World!"
+#@app.route('/')
+#def hello():
+#    #return render_template('index.html')
+#    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+#    app.run()
+from app import app
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
